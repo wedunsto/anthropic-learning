afc6fa7b1c42f09dfa8c322683cd2de39ea307e7
William Dunston <wedunsto@aggies.ncat.edu>
2026-07-09 10:36:59 -0400

    Added the boilerplate for the prompt evaluations exercise

--- main.py
+++ main.py
@@ -1,8 +1,58 @@
 from dotenv import load_dotenv
 from anthropic import Anthropic
+from prompt_evaluations_exercise import PromptEvaluations
 
 load_dotenv()
 
 client = Anthropic()
-model = "claude-sonnet-4-0"
+model = "claude-sonnet-5"
 
+promptEvaluations = PromptEvaluations(client, model)
+
+# Prompt engineering rules to replace prefilling
+promptRules = """
+Rules:
+- Return only plain text
+- Do not use markdown
+- Do not include comments
+- Do not include explanations
+- After the plain text, write END_OF_COMMANDS
+"""
+
+# Tasks for the prompt evaluation exercise
+tasks = [
+    {
+        "task": "Create a Python function to extract the AWS account ID from an ARN"
+    },
+    {
+        "task": "Write a JSON policy document that allows read-only access to a specific S3 bucket"
+    },
+    {
+        "task": "Write a regular expression that validates whether a string is in a valid email address format"
+    }
+]
+
+"""
+Starting point for grading and improving prompts
+Add the task to the f-string
+Add prompt rules after the task to replace prefilling
+"""
+initial_prompt = f"""
+Please provide a solution to the following task:
+{tasks[0]["task"]}
+{promptRules}
+"""
+
+promptEvaluations.storeUserInputs(initial_prompt)
+
+# Provide Claude with context to customize how Claude responds to user input
+system_prompt = """
+You are an expert regarding AWS and how to create Python function, JSON policy documents,
+and regular expressions that interact with AWS. You adhere to industry best practices
+"""
+
+promptEvaluations.stop_sequences.append("END_OF_COMMANDS")
+
+claudeResponse = promptEvaluations.askClaude(system_prompt)
+
+promptEvaluations.storeClaudeResponse(claudeResponse)
---
+++ prompt_evaluations_exercise.py
@@ -0,0 +1,55 @@
+"""
+Objective: Improve the Model grader that was discussed in the Prompt Evaluation section
+Give the Model Grader more context on what a good solution looks like
+"""
+class PromptEvaluations:
+    def __init__(self, client, model):
+        self.client = client
+        self.model = model
+        self.messages = []
+        self.stop_sequences = []
+
+    # Store user inputs to the message history
+    def storeUserInputs(self, message):
+        self.messages.append({"role": "user", "content": message})
+
+    # Store Claude's response to the message history
+    def storeClaudeResponse(self, response):
+        self.messages.append({"role": "assistant", "content": response})
+
+    # Stream Claude's output and get the the final message
+    def streamClaudeResponse(self, parameter):
+        with self.client.messages.stream(**parameter) as stream:
+            for text in stream.text_stream:
+                print(text, end="")
+            
+            final_message = stream.get_final_message()
+
+        text_block = [
+            block.text
+            for block in final_message.content
+                if block.type == "text"
+        ]
+
+        return text_block
+
+    """
+    Get Claude's response to the latest user's input 
+    with the context of the message history
+    """
+    def askClaude(self, system_prompt=None):
+        parameters = {
+            "model": self.model,
+            "max_tokens": 1000,
+            "messages": self.messages
+        }
+
+        if system_prompt:
+            parameters["system"] = system_prompt
+
+        if self.stop_sequences:
+            parameters["stop_sequences"] = self.stop_sequences
+
+        claude_response = self.streamClaudeResponse(parameters)
+
+        return claude_response
